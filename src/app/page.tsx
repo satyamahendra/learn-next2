cccba243e8fabc110b0b03a9af1ef1fde87cd544
satyamahendra <satyamahendra09@gmail.com>
2024-04-21 16:55:38 +0700

    ui scaffolding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,15 @@
-import Link from "next/link";
+import Image from "next/image";
 
 export default function HomePage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
-    </main>
-  );
+
+	const imagesArray = ["https://utfs.io/f/b8fa4382-9d63-48d3-8266-ad88e60f084f-14je.png", "https://utfs.io/f/d49d3727-01fc-4a69-a6f9-f591ad92560f-8iyzmm.jpg", "https://utfs.io/f/2b237895-37b2-42c7-8c56-3592c28d18bd-r2kr59.jpg"]
+
+	return (
+		<main className="min-h-screen p-6">
+			<h1 className="text-center w-full">Your Gallery</h1>
+			<div className="flex  h-40 w-40">
+				{imagesArray.map((image, index) => <img src={image} alt={`image-${index}`} className="" />)}
+			</div>
+		</main>
+	);
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+export default function Navbar() {
+    return (
+        <nav className="p-6 border-b">
+            <ul className="flex items-center gap-6">
+                <li>T3Gallery</li>
+                <li className="ml-auto">Home</li>
+                <li>Sign in</li>
+            </ul>
+        </nav>
+    )
+}
